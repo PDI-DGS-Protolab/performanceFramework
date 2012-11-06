3a3b4d31d8e77fd52a37fca75a14c85db976d474
david <david@martell.(none)>
2012-11-06 09:32:25 +0100

    WebGL gui updated

--- public/js/Connector.js
+++ public/js/Connector.js
@@ -1,246 +1,299 @@
 
 (function (PBDV, io, undefined) {
 
-	"use strict";
+    "use strict";
+
+
+    /** 
+     *  @class Connector
+     *  @constructor
+     *  @param org {Organizer} The organizer object which will receive the Connector requests after an event is handled
+     */
+    var Connector = function ( org ) {
+        
+        /* Attributes */
 
+        /**
+         *  The Organizer object which direct the app
+         *  @property organizer
+         *  @type Organizer
+         */
+        this.organizer = org;
+
 
-	/* Constructor */
+        /**
+         *  A list to save the current final version of each test
+         *  @property versions
+         *  @type array
+         */
+        this.versions = [];
 
-	var Connector = function(org) {
-		
-		/* Attributes */
 
-		// The Organizer object which direct the app
-		this.organizer = org;
+        /**
+         *  The WebSocket used to receive/sent data from/to the server in real-time
+         *  @property socket
+         *  @type Object
+         */
+        this.socket = null;
+        
 
-		// A list to save the current final version of each test
-		this.versions = [];
+        /* Initialization */
+        
+        var url = window.location.href;
+        this.connect( url );    
 
-		// The WebSocket used to receive/sent data from/to the server in real-time
-		this.socket;
-		
+    };
 
-		/* Initialization */
-		
-		var url = window.location.href;
-		this.connect( url );	
-	}
 
 
+    /* Private Methods */
 
-	/* Private Methods */
+    /**
+     *  Auxiliary method to handle an event when a new point comes from the server
+     *  @method setupNewPointEvent
+     *  @private
+     *  @param conn {Connector}
+     */
+    var setupNewPointEvent = function ( conn ) {
 
-	/*
-	 *
-	 */
-	var setupNewPointEvent = function(conn) {
+        conn.socket.on('newPoint', function (data) {
 
-		conn.socket.on('newPoint', function (data) {
+            if ( data.err ) {
+                // No points received, so we report to the UI
+                var time = new Date().toTimeString().slice(0, 8);
+                var msg  = PBDV.Constants.Message.NO_POINTS_RECEIVED;
+                conn.organizer.log( time, msg );
 
-			if ( data.err ) {
-				// No points received, so we report to the UI
-				var time = new Date().toTimeString().slice(0, 8);
-				var msg  = PBDV.Constants.Message.NO_POINTS_RECEIVED;
-				conn.organizer.log( time, msg );
+            } else if ( data.message ) {
 
-			} else if ( data.message ) {
+                // Sending the data received to the corresponding test
+                var id = data.message.id;
+                if ( data.version === conn.versions[id] ) {
+                    conn.organizer.addData( id, data.message.point );
+                }
+                
+            }
 
-				// Sending the data received to the corresponding test
-				var id = data.message.id;
-				if ( data.version === conn.versions[id] ) {
-					conn.organizer.addData( id, data.message.point );
-				}
-				
-			}
+        });
 
-		});
+    };
 
-	}
 
+    /**
+     *  Auxiliary method to handle one event per plot defined in the constants file
+     *  @method setupPlotsEvents
+     *  @private
+     *  @param conn {Connector}
+     */
+    var setupPlotsEvents = function ( conn ) {
 
-	/*
-	 *
-	 */
-	var setupPlotsEvents = function(conn) {
+        var plotCallback = function ( plot ) {
+            var pl = plot;
 
-		// For each plot event, we add the data to the corresponding plot
-		for ( var p in PBDV.Constants.Plots.Components ) {
-			var plot = p.toLowerCase();
-			
-			(function() {
-				var pl = plot;
-				// Listening to every event about our configured plots
-				conn.socket.on( pl, function (data) {
-					conn.organizer.addDataPlots( data.host, data.time, data[pl], pl );
-				});
-			})();
-		}
+            // Listening to every event about our configured plots
+            conn.socket.on( pl, function (data) {
+                conn.organizer.addDataPlots( data.host, data.time, data[pl], pl );
+            });
+        };
 
-	}
 
+        // For each plot event, we add the data to the corresponding plot
+        var Comps = PBDV.Constants.Plots.Components;
+        for ( var p in Comps ) {
+            if ( Comps.hasOwnProperty(p) ) {
+                var plot = p.toLowerCase();
+                plotCallback( plot );
+            }
+        }
 
-	/*
-	 *
-	 */
-	var setupErrorEvents = function(conn) {
+    };
 
-		// Rename
-		var Message = PBDV.Constants.Message;
 
+    /**
+     *  Auxiliary method to handle several possible error events
+     *  @method setupErrorEvents
+     *  @private
+     *  @param conn {Connector}
+     */
+    var setupErrorEvents = function ( conn ) {
 
-		// Auxiliary function to show the extraordinary events
-		var showInfoMessage = function(str) {
-			var date = new Date().toTimeString().slice(0, 8);
-			conn.organizer.log(date, str);
-		}
+        // Rename
+        var Message = PBDV.Constants.Message;
 
 
-		// Listening to the corresponding events
+        // Auxiliary function to show the extraordinary events
+        var showInfoMessage = function (str) {
+            var date = new Date().toTimeString().slice(0, 8);
+            conn.organizer.log(date, str);
+        };
 
-		conn.socket.on('error', function(data) {
-			showInfoMessage( Message.SOCKET_ERROR );
-		});
 
+        // Listening to the corresponding events
 
-		conn.socket.on('disconnect', function(data) {
-			showInfoMessage( Message.CLIENT_DISCONNECT );
-		});
+        conn.socket.on('error', function () {
+            showInfoMessage( Message.SOCKET_ERROR );
+        });
 
 
-		conn.socket.on('reconnect_failed', function() {
-			showInfoMessage( Message.CLIENT_COULD_NOT_RECONNECT );
-		});
+        conn.socket.on('disconnect', function () {
+            showInfoMessage( Message.CLIENT_DISCONNECT );
+        });
 
 
-		conn.socket.on('reconnect', function() {
-			showInfoMessage( Message.SUCCESSFUL_RECONNECTION );
-		});
-	
+        conn.socket.on('reconnect_failed', function () {
+            showInfoMessage( Message.CLIENT_COULD_NOT_RECONNECT );
+        });
 
-		conn.socket.on('reconnecting', function () {
-			showInfoMessage( Message.TRYING_TO_RECONNECT );
-		});
 
-	}
+        conn.socket.on('reconnect', function () {
+            showInfoMessage( Message.SUCCESSFUL_RECONNECTION );
+        });
+    
 
+        conn.socket.on('reconnecting', function () {
+            showInfoMessage( Message.TRYING_TO_RECONNECT );
+        });
 
-	/*
-	 *
-	 */
-	var setupEvents = function(conn) {
+    };
 
-		conn.socket.on('init', function (data) {
 
-			// 
-			var nagents  = data.agents.nAgents;
-			var interval = data.agents.interval * 1000;
-		
-			// Updating the versions because agents could be launched before
-			for (var t in data.tests) {
-				var v = data.tests[t].version;
-				conn.versions.push( v );
-			}
+    /**
+     *  Method used to listen to events received by the socket
+     *  @method setupEvents
+     *  @private
+     *  @param conn {Connector}
+     */
+    var setupEvents = function ( conn ) {
 
-			// Initializing Drawer and 3D Test
-			conn.organizer.configTest( data.tests );
+        conn.socket.on('init', function (data) {
 
-			// Initializing 2D Plots Axis
-			conn.organizer.configPlots( interval, nagents, data.hosts );
+            var nagents  = data.agents.nAgents;
+            var interval = data.agents.interval * 1000;
+            var tests    = data.tests;
 
-			// Setting up the event handler when new points come from the server
-			setupNewPointEvent(conn);
-		});
+            // Updating the versions because agents could be launched before
+            for (var t in tests) {
+                if ( tests.hasOwnProperty(t) ) {
+                    var v = tests[t].version;
+                    conn.versions.push( v );
+                }
+            }
 
+            // Initializing Drawer and 3D Test
+            conn.organizer.configTest( tests );
 
-		conn.socket.on('endLog', function (data) {
-			conn.organizer.log( data.time, data.message, data.host );
-		});
+            // Initializing 2D Plots Axis
+            conn.organizer.configPlots( interval, nagents, data.hosts );
 
+            // Setting up the event handler when new points come from the server
+            setupNewPointEvent( conn );
+        });
 
-		// 
-		setupPlotsEvents(conn);
-		
 
-		//
-		setupErrorEvents(conn);
+        conn.socket.on('endLog', function (data) {
+            conn.organizer.log( data.time, data.message, data.host );
+        });
 
-	}
 
+        // 
+        setupPlotsEvents(conn);
+        
 
-	/* Public API */
+        //
+        setupErrorEvents(conn);
 
-	Connector.prototype = {
-		
-		/*
-		 *
-		 */
-		connect : function( url ) {
+    };
 
-			// Connecting to the server
-			this.socket = io.connect( url, {
-				'connect timeout' : 10000
-			});
 
-			this.socket.emit('init');
+    /* Public API */
 
-			// Attaching events to the socket
-			setupEvents( this );
-		},
+    Connector.prototype = {
+        
+        /**
+         *  Method to establish a new connection with a server through the Web Socket
+         *  @method connect
+         *  @param url {string}
+         */
+        connect : function ( url ) {
 
+            // Connecting to the server
+            this.socket = io.connect( url, {
+                'connect timeout' : 10000
+            });
 
-		/*
-		 *
-		 */
-		startTest : function(num) {
-			this.socket.emit('newTest', { id : num });
-		},
+            this.socket.emit('init');
 
+            // Attaching events to the socket
+            setupEvents( this );
 
-		/*
-		 *
-		 */
-		restartTest : function(num) {			
-			this.versions[num] += 1;
+        },
 
-			this.pauseTest(num);
-			this.startTest(num);
-		},
 
+        /**
+         *  Method to start a new test
+         *  @method startTest
+         *  @param num {number} The test number
+         */
+        startTest : function ( num ) {
+            this.socket.emit('newTest', { id : num });
+        },
 
-		/*
-		 *
-		 */
-		pauseTest : function(num) {
-			this.socket.emit('pauseTest', { id : num });
-		},
 
+        /**
+         *  Method to restart a new test
+         *  @method restartTest
+         *  @param num {number} The test number
+         */
+        restartTest : function ( num ) { 
 
-		/*
-		 *
-		 */
-		continueTest : function(num) {
-			this.socket.emit('continueTest', { id : num });
-		},
+            this.versions[num] += 1;
 
+            this.pauseTest(num);
+            this.startTest(num);
+            
+        },
 
-		/*
-		 *
-		 */
-		addNewEvent : function(event, callback) {
-			// The socket will listen to and handle 'event' with a pre-defined 'callback'
-			if (typeof event === "string" && typeof callback === "function") {
-				this.socket.on(event, callback);	
-			}
-		}
 
-	}; // prototype
+        /**
+         *  Method to pause a new test
+         *  @method pauseTest
+         *  @param num {number} The test number
+         */
+        pauseTest : function ( num ) {
+            this.socket.emit('pauseTest', { id : num });
+        },
 
 
-	// Exported to the namespace
-	PBDV.Connector = Connector;
+        /**
+         *  Method to continue a new test
+         *  @method continueTest
+         *  @param num {number} The test number
+         */
+        continueTest : function ( num ) {
+            this.socket.emit('continueTest', { id : num });
+        },
 
-//})( window.PBDV = window.PBDV || {});	// Namespace
 
-})( window.PBDV = window.PBDV || {},	// Namespace
-	io);								// Dependencies
+        /**
+         *  Method to let clients of this API to add new events received by the WebSocket
+         *  @method addNewEvent
+         *  @param event {string}
+         *  @param callback {function}
+         */
+        addNewEvent : function ( event, callback ) {
 
+            if (typeof event === "string" && typeof callback === "function") {
+                this.socket.on(event, callback);    
+            }
+
+        }
+
+    }; // prototype
+
+
+    // Exported to the namespace
+    PBDV.Connector = Connector;
+
+
+})( window.PBDV = window.PBDV || {},    // Namespace
+    io);                                // Dependencies
--- public/js/Drawer.js
+++ public/js/Drawer.js
@@ -1,303 +1,357 @@
 
 (function ( PBDV, THREE, undefined ) {
 
-	"use strict";
+    "use strict";
+
+
+    /**
+     * @class Drawer
+     * @constructor
+     */
+    var Drawer = function () {
+
+        /* Attributes */
+
+        /**
+         * The array which contains the created scenes by the drawer
+         * @property arrayScenes
+         * @type array
+         */
+        this.arrayScenes   = [];
+
+
+        /**
+         * The array which contains the created cameras by the drawer
+         * There must be the same number of scenes and cameras (one camera per scene)
+         * @property arrayCameras
+         * @type array
+         */
+        this.arrayCameras  = [];
+
+
+        /**
+         * The number of the scene that is currently being rendered
+         * @property currentScene
+         * @type number
+         * @default 0
+         */
+        this.currentScene  = 0;
+
+
+        /**
+         * The canvas object where the WebGL render draws
+         * @property canvas
+         * @type DOMObject
+         */
+        this.canvas = PBDV.Constants.ViewController.DOM.visualizator;
+        
 
+        /**
+         * The THREE renderer object
+         * @property threeRenderer
+         * @type THREE.Renderer
+         */
+        this.threeRenderer = this.createRenderer();
 
-	/* Constructor */
 
-	var Drawer = function() {
+        /* Initialization */
 
-		/* Attributes */
+        var ctx = this;
+        window.addEventListener( 'resize', function () {
+            onWindowResize.call(ctx);
+        }, false);
 
-		//
-		this.arrayScenes   = [];
+    };
 
-		//
-		this.arrayCameras  = [];
 
-		//
-		this.threeRenderer = null;
 
-		// The scene that is currently being rendered
-		this.currentScene  = 0;
+    /* Private Methods */
+
+    /**
+     * Auxiliary function to support shim RequestAnimationFrame
+     * @method requestAnimationFrame
+     * @private
+     * @param callback {function} The callback perform by the RAF
+     */
+    var requestAnimationFrame = function ( callback ) {
 
-		//
-		this.canvas = PBDV.Constants.ViewController.DOM.visualizator;
+        // Getting the actual RAF function depending on the used browser and its support
+        var f = window.requestAnimationFrame || 
+        window.webkitRequestAnimationFrame   || 
+        window.mozRequestAnimationFrame      || 
+        window.oRequestAnimationFrame        || 
+        window.msRequestAnimationFrame       || 
+        function ( callback ) {
+            window.setTimeout( callback, 1000 / 60 );
+        };
 
+        // Running the callback animation with the RAF function
+        f(callback);
 
-		/* Initialization */
-		
-		this.threeRenderer = this.createRenderer();
+    };
 
-		var ctx = this;
-		window.addEventListener( 'resize', function() {
-			onWindowResize.call(ctx);
-		}, false);
-	}
 
+    /**
+     * Event called when the window changed its size
+     * Method invoked with the Drawer context
+     * @event onWindowResize
+     * @private
+     */
+    var onWindowResize = function () {
+        
+        if ( this.arrayCameras ) {
 
+            // Updating the aspect of the current camera 
+            var camera = this.arrayCameras[ this.currentScene ];
+            var aspect = this.canvas.width() / this.canvas.height();
+            camera.updateAspect( aspect );
+        }
 
-	/* Private Methods */
+        // Updating the render size
+        this.threeRenderer.setSize( this.canvas.width(), this.canvas.height() );
 
-	/*
-	 * Auxiliary function to support shim RequestAnimationFrame
-	 */
-	var requestAnimationFrame = function( callback ) {
+    };
 
-		// Getting the actual RAF function depending on the used browser and its support
-		var f = window.requestAnimationFrame || 
-		window.webkitRequestAnimationFrame   || 
-		window.mozRequestAnimationFrame      || 
-		window.oRequestAnimationFrame        || 
-		window.msRequestAnimationFrame       || 
-		function( callback, element) {
-			window.setTimeout( callback, 1000 / 60 );
-		};
 
-		// Running the callback animation with the RAF function
-		f(callback);
+    /**
+     * Event called when the mouse entered in the render region
+     * Method invoked with the Drawer context
+     * @event onMouseEnter
+     * @private
+     */
+    var onMouseEnter = function () {
+        this.arrayCameras[ this.currentScene ].enableControls();
+    };
 
-	}
 
+    /**
+     * Event called when the mouse went out of the render region
+     * Method invoked with the Drawer context
+     * @event onMouseOut
+     * @private
+     */ 
+    var onMouseOut = function () {
 
-	/*
-	 * Event called when the window changed its size
-	 * Method invoked with the Drawer context
-	 */
-	var onWindowResize = function() {
-		
-		if ( this.arrayCameras ) {
+        for (var i = 0; i < this.arrayCameras.length; i++) {
+            this.arrayCameras[i].disableControls();
+        }
 
-			// Updating the aspect of the current camera 
-			var camera = this.arrayCameras[ this.currentScene ];
-			var aspect = this.canvas.width() / this.canvas.height();
-			camera.updateAspect( aspect );
-		}
+    };
 
-		// Updating the render size
-		this.threeRenderer.setSize( this.canvas.width(), this.canvas.height() );
+    
 
-	}
+    /* Public API */
 
+    Drawer.prototype = {
 
-	/*
-	 * Event called when the mouse entered in the render region
-	 * Method invoked with the Drawer context
-	 */	
-	var onMouseEnter = function() {
-		this.arrayCameras[ this.currentScene ].enableControls();
-	}
+        // Methods invoked by the Drawer itself
 
+        /**
+         * Method which performs the animation loop
+         * @method animate
+         */ 
+        animate : function () {
 
-	/*
-	 * Event called when the mouse went out of the render region
-	 * Method invoked with the Drawer context
-	 */
-	var onMouseOut = function() {
+            // Animation loop executed just when the user is viewing the tab
+            var ctx = this;
+            requestAnimationFrame(function () {
+                return ctx.animate.call(ctx);
+            });
 
-		for (var i = 0; i < this.arrayCameras.length; i++) {
-			this.arrayCameras[i].disableControls();
-		}
+            // Animating the current rendered scene and camera
+            var scene  = this.arrayScenes[ this.currentScene ];
+            var camera = this.arrayCameras[ this.currentScene ];
 
-	}
+            camera.animate();
+            scene.animate( camera.threeCamera );
+        
+            this.render.call(this);
 
-	
+        },
 
-	/* Public API */
 
-	Drawer.prototype = {
+        /**
+         * Creates the cameras needed by the scenes
+         * @method createCameras
+         */ 
+        createCameras : function () {
 
-		// Methods invoked by the Drawer itself
+            // Defining the callback that will be passed to the Camera controls
+            var renderCallback = function () {
+                return ctx.render.call(ctx);
+            };
 
-		/*
-		 *
-		 */
-		animate : function() {
+            // Loop for the Cameras creation
+            for (var i = 0; i < this.arrayScenes.length; i++) {
 
-			// Animation loop executed just when the user is viewing the tab
-			var ctx = this;
-			requestAnimationFrame(function() {
-				return ctx.animate.call(ctx);
-			});
+                var aspect = this.canvas.width() / this.canvas.height();
+                var camera = new PBDV.Camera( aspect );
 
-			// Animating the current rendered scene and camera
-			var scene  = this.arrayScenes[ this.currentScene ];
-			var camera = this.arrayCameras[ this.currentScene ];
+                // Enabling the controls for each camera except the first one
+                var ctx = this;
+                camera.setControls( renderCallback );
 
-			camera.animate();
-			scene.animate( camera.threeCamera );
-		
-			this.render.call(this);
+                if ( i !== 0 ) {
+                    camera.disableControls();
+                }
 
-		},
+                // Storing each camera
+                this.arrayCameras.push( camera );
+            }
 
+            var self = this;
 
-		/*
-		 *
-		 */
-		createCameras : function() {
+            // Launching mouse events to enable/disable the camera controls
+            this.canvas.mouseenter(function () {
+                onMouseEnter.call(self);
+            });
 
-			for (var i = 0; i < this.arrayScenes.length; i++) {
+            this.canvas.mouseout(function () {
+                onMouseOut.call(self);
+            });
 
-				// Creating a new camera with the current aspect
-				var aspect = this.canvas.width() / this.canvas.height();
-				var camera = new PBDV.Camera( aspect );
+        },
 
-				// Enabling the controls for each camera except the first one
-				var ctx = this;
-				camera.setControls( function() {
-					return ctx.render.call(ctx);
-				});
 
-				if ( i !== 0 ) {
-					camera.disableControls();
-				}
+        /**
+         * Creates the 3D renderer and will draw inside of the canvas
+         * @method createRenderer
+         * @param canvas {DOMObject} The canvas which should replace the current one
+         */ 
+        createRenderer : function ( canvas ) {
 
-				// Storing each camera
-				this.arrayCameras.push( camera );
-			}
+            if ( canvas ) {
+                this.canvas = canvas;
+            }
 
-			var self = this;
+            // Creating the WebGL Render and setting its size and color
+            var threeRenderer = new THREE.WebGLRenderer({
+                antialias : true
+            });
 
-			// Launching mouse events to enable/disable the camera controls
-			this.canvas.mouseenter(function() {
-				onMouseEnter.call(self);
-			});
+            threeRenderer.setSize( this.canvas.width(), this.canvas.height() ); 
+            threeRenderer.setClearColorHex(0xEEEEEE, 1.0);
+            return threeRenderer;
 
-			this.canvas.mouseout(function() {
-				onMouseOut.call(self);
-			});
+        },
 
-		},
 
+        /**
+         * Creates the scenes needed for each test
+         * @method createScenes
+         * @param tests {object} The test information needed for the graphs creation
+         */ 
+        createScenes : function ( tests ) {
 
-		/*
-		 *
-		 */
-		createRenderer : function( canvas ) {
+            // Rename
+            var Constants = PBDV.Constants.Drawer;
 
-			if ( canvas ) {
-				this.canvas = canvas;
-			}
+            var scene1 = new PBDV.Scene({
+                size   : Constants.SIZE_MAP,
+                titles : Constants.Test.Provision,
+                test   : tests.push
+            });
 
-			// Creating the WebGL Render and setting its size and color
-			var threeRenderer = new THREE.WebGLRenderer({
-				antialias : true
-			});
+            var scene2 = new PBDV.Scene({
+                size   : Constants.SIZE_MAP,
+                titles : Constants.Test.Pop,
+                test   : tests.pop
+            });
 
-	        threeRenderer.setSize( this.canvas.width(), this.canvas.height() ); 
-			threeRenderer.setClearColorHex(0xEEEEEE, 1.0);
-			return threeRenderer;
+            this.arrayScenes = [ scene1, scene2 ];
 
-		},
+        },
 
 
-		/*
-		 *
-		 */
-		createScenes : function( tests ) {
+        /**
+         * The method to render the current scene with its corresponding camera
+         * @method render
+         */ 
+        render : function () {
 
-			// Rename
-			var Constants = PBDV.Constants.Drawer;
+            var scene  = this.arrayScenes[ this.currentScene ];
+            var camera = this.arrayCameras[ this.currentScene ];
 
-			var scene1 = new PBDV.Scene({
-				size   : Constants.SIZE_MAP,
-				titles : Constants.Test.Provision,
-				test   : tests.push
-			});
+            camera.lookAt( scene.graph.position() );
 
-			var scene2 = new PBDV.Scene({
-				size   : Constants.SIZE_MAP,
-				titles : Constants.Test.Pop,
-				test   : tests.pop
-			});
+            this.threeRenderer.render( scene.threeScene, camera.threeCamera );
 
-			this.arrayScenes = [ scene1, scene2 ];
+        },
 
-		},
 
+        // Methods invoked by the Organizer
 
-		/*
-		 *
-		 */
-		render : function() {
+        /**
+         * To add a new point sent by the connector to the current scene.
+         * The drawer delegates this task in order to get a scene which add the point to its inner elements
+         * @method addData
+         * @param testNumber {number} The test ID
+         * @param point {object} The received point to be added into the scene
+         */ 
+        addData : function ( testNumber, point ) {
 
-			var scene  = this.arrayScenes[ this.currentScene ];
-			var camera = this.arrayCameras[ this.currentScene ];
+            var scene = this.arrayScenes[ testNumber ];
+            scene.addDataToGraph( point );
 
-			camera.lookAt( scene.graph.position() );
+        },
 
-			this.threeRenderer.render( scene.threeScene, camera.threeCamera );
 
-		},
+        /**
+         * Updates the drawer to the new current scenes asked by the user
+         * @method changeToTest
+         * @param testNumber {number} The test ID
+         */ 
+        changeToTest : function ( testNumber ) {
 
+            this.currentScene = testNumber;
+            onWindowResize.call(this);
 
-		// Methods invoked by the Organizer
+        },
 
-		/*
-		 *
-		 */
-		addData : function( testNumber, point ) {
 
-			var scene = this.arrayScenes[ testNumber ];
-			scene.addDataToGraph( point );
+        /**
+         * Configures the drawer creating the scenes and cameras needed and performs the animation loop
+         * @method changeToTest
+         * @param tests {number} The test ID
+         */ 
+        configTest : function ( tests ) {
 
-		},
+            this.createScenes( tests );
+            this.createCameras();
 
+            this.animate();
 
-		/*
-		 *
-		 */
-		changeToTest : function( testNumber ) {
+        },
 
-			this.currentScene = testNumber;
-			onWindowResize.call(this);
 
-		},
+        /*
+         * Method to return the DOM element
+         * @method DOMElement
+         * @return the DOM element provided by the renderer
+         */
+        DOMElement : function () {
+            return this.threeRenderer.domElement;
+        },
 
 
-		/*
-		 *
-		 */
-		configTest : function( tests ) {
+        /**
+         * Restarts the scene of the given test
+         * @method restart
+         * @param testNumber {number} The test ID
+         */ 
+        restart : function ( testNumber ) {
 
-			//
-			this.createScenes( tests );
+            var scene = this.arrayScenes[ testNumber ];
+            scene.restart();
 
-			//
-			this.createCameras();
+        }
 
-			//
-			this.animate();
+    }; // prototype
 
-		},
 
+    // Exported to the namespace
+    PBDV.Drawer = Drawer;
 
-		/*
-		 *
-		 */
-		DOMElement : function() {
-			return this.threeRenderer.domElement;
-		},
 
-
-		/*
-		 *
-		 */
-		restart : function( testNumber ) {
-
-			var scene = this.arrayScenes[ testNumber ];
-			scene.restart();
-
-		}
-
-	}; // prototype
-
-
-	// Exported to the namespace
-	PBDV.Drawer = Drawer;
-
-
-})( window.PBDV = window.PBDV || {}, 	// Namespace
-	THREE);								// Dependencies
+})( window.PBDV = window.PBDV || {},    // Namespace
+    THREE);                             // Dependencies
