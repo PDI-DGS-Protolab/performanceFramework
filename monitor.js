5244f3d0518dae81de985aa62c173cea4db8c5be
Aitor Magán <amagan@conwet.com>
2012-11-21 12:11:09 +0100

    Fix some bugs in bootstrap template. monitor.js has been unupdated

--- monitor.js
+++ monitor.js
@@ -2,37 +2,41 @@ var childProcess = require('child_process');
 var utils = require('./utils.js');
 var net = require('net');
 var os = require('os');
+var cluster = require('cluster');
 
-var paths = [];
+var paths = new Array();
 
 for (var i = 2; i < process.argv.length; i++) {
     paths.push(process.argv[i]);
 }
 
-var programs = [];
-var pid;
-var pids = [];
-programs = createProcessList(paths);
+paths = paths.sort();
 
-for (var i = 0; i < programs.length; i++) {
-    var aux = new Array()
-    pid = runProgram(programs[i].process);
-    console.log('A new process has been launched with PID: ' + pid);
+var names = [];
+names = splitAndName(paths);
+console.log(names[0]);
 
-    setTimeout(function () {
-        aux = utils.getchildProcesses(pid);
-        aux.push(pid);
-        pids.push(aux);
-    }, 1000);
-}
+server = net.createServer(function (connection) {
 
-var server = net.createServer(function (connection, programs) {
-
+    var pid;
     var monitorInterval;
+    var pids = new Array();
 
     if (server.connections === 1) {
 
         console.log('Client open the connection...');
+
+        for (var i = 0; i < paths.length; i++) {
+            var aux = new Array()
+            pid = createAgent(paths[i]);
+            console.log('A new program has been launched with PID: ' + pid);
+
+            setTimeout(function () {
+                aux = utils.getchildProcesses(pid);
+                aux.push(pid);
+                pids.push(aux);
+            }, 1000);
+        }
 
         //Monitoring an agent sending the client information about the usage of CPU and RAM
         monitorInterval = setInterval(function () {
@@ -41,7 +45,7 @@ var server = net.createServer(function (connection, programs) {
                     var res = utils.monitor(pids[i], function (res) {
                         console.log('CPU: ' + res.cpu + ' - Memory: ' + res.memory);
                         console.log(names[0]);
-                        connection.write(JSON.stringify({host: os.hostname(), name: programs[i], cpu: {percentage: res.cpu}, memory: {value: res.memory}}) + '\n');
+                        connection.write(JSON.stringify({host: os.hostname(), name: names[i], cpu: {percentage: res.cpu}, memory: {value: res.memory}}) + '\n');
                     });
                 }
                 iterate(i);
@@ -55,6 +59,7 @@ var server = net.createServer(function (connection, programs) {
         connection.on('end', function () {
             console.log('Client closed connection...');
             clearInterval(monitorInterval);
+            process.kill(pid);
             connection.end();
 
         });
@@ -65,40 +70,44 @@ var server = net.createServer(function (connection, programs) {
 }).listen(8091);
 
 /**
- * Runs a process
- * @return The PID of the process
+ * Creates an agent
+ * @return The PID of the agent
  */
-function runProgram(prog, arguments){
-    var child = childProcess.spawn(prog, arguments);
+var createAgent = function (path) {
+    var child = childProcess.fork(path);
     var pid = child.pid;
     return pid;
 }
 
-function splitAndName(path) {
-    var elems = [];
-    var res;
-    elems = path.split('/');
-    res = elems[elems.length - 1];
-    return res;
-}
+function splitAndName(programs) {
 
-function createProcessList(paths){
-    var elems = [];
-    var programsWithArgs = [];
-    var programs = [];
+    var listRes = [];
 
-    for(var i = 2; i < process.argv.length; i++){
-        elems = process.argv[i].split(' ');
-        if(elems.length > 1){
-            programsWithArgs.push({process: elems[0], name: elems[0], arguments: elems.slice(1)});
-        }
-        if(elems.length === 1){
-            programs.push({process: elems[0], name: splitAndName(elems[0])});
-        }
+    for (var i = 0; i < programs.length; i++) {
+        var elems = programs[i].split('/');
+        elems = elems[elems.length - 1].split('.');
+        var res = elems[0];
+        listRes.push(res);
     }
 
-    programs = programs.concat(programsWithArgs);
-    return programs;
+    listRes = listRes.sort();
+    var aux = listRes[1];
+    var cont = 1;
+
+    for (var i = 0; i < listRes.length; i++) {
+        if (aux === listRes[i]) {
+            listRes[i] = listRes[i] + cont.toString();
+            cont++;
+        }
+        else {
+            cont = 1;
+            aux = listRes[i];
+            listRes[i] = listRes[i] + cont.toString();
+            cont++;
+        }
+    }
+    console.log(listRes);
+    return listRes;
 }
 
 process.on('uncaughtException', function onUncaughtException(err) {
