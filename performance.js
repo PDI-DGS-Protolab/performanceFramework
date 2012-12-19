e7a53419c46298677cb1f1e06917a262de3c7bcf
Fernando Godino <fgodino@conwet.com>
2012-12-19 12:05:45 +0100

    error log when no monitors found

--- performance.js
+++ performance.js
@@ -138,13 +138,13 @@ var createAndLaunchMonitors = function (hosts, clients) {
     var i = 0;
     var CPU_Mem = this.CPU_Mem;
     var start = this.start;
-
+    console.log(hosts.length);
     for (i = 0; i < hosts.length; i++) {
         var host = hosts[i];
         var client = new net.Socket();
-
+        console.log("entra");
         clients.push(client);
-        client.connect(8091, host, function (client) {
+        client.connect(8091, host, function () {
             client.on('data', function (data) {
 
                 var splitted = data.toString().split('\n');
@@ -163,11 +163,11 @@ var createAndLaunchMonitors = function (hosts, clients) {
                 }
             });
 
-        }.bind({}, client));
-
-        client.on('error', function (err) {
-            console.log(err);
         });
+
+        client.on('error', function (host,err) {
+            console.log('monitor on \'' + host +'\' is not connected or is not working');
+        }.bind(null,host));
     }
 };
 
